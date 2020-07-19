d5544472b249ec786e1f05bf43c643d43bf99c7f
Frank Drewett <fdrewett@gmail.com>
2020-07-19 14:56:17 -0600

    init

---
+++ scan-network-both-arp-nmap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ip_address=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
+echo $ip_address
+stripped_last_octet=`echo $ip_address | cut -d"." -f1-3`
+echo $stripped_last_octet
+subnet=$stripped_last_octet".0"
+echo $subnet
+sudo arp-scan $subnet/24
+sudo nmap $subnet/24
