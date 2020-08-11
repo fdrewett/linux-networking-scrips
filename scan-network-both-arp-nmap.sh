e2750cba57338f994fb6c35851a4557a22ec8d7e
Frank Drewett <fdrewett@gmail.com>
2020-08-10 20:54:46 -0600

    add dividers to the results

--- scan-network-both-arp-nmap.sh
+++ scan-network-both-arp-nmap.sh
@@ -5,5 +5,15 @@ stripped_last_octet=`echo $ip_address | cut -d"." -f1-3`
 echo $stripped_last_octet
 subnet=$stripped_last_octet".0"
 echo $subnet
+echo "----------------------------------------------"
+echo "arp scan results - $subnet"/24
+echo "----------------------------------------------"
 sudo arp-scan $subnet/24
+echo "----------------------------------------------"
+echo "nmap quick scan results - $subnet"/24
+echo "----------------------------------------------"
+sudo nmap -sn $subnet/24
+echo "----------------------------------------------"
+echo "nmap full scan results - $subnet"/24
+echo "----------------------------------------------"
 sudo nmap $subnet/24
